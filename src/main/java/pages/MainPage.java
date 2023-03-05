24b4849067c0c7b1c2c607dab1c93be58b4c4f65
Mariya Leshcheva <maleshch@icloud.com>
2023-03-05 14:02:37 +0300

    Changed the getAnswer method. The checkQuestion test has been changed. Added parameterization.

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -19,31 +18,21 @@ public class MainPage {
     //Локатор кнопки "Заказать"
     private final By buttonOrder  = By.className("Button_Button__ra12g");
 
-    //Локатор вопроса в разделе "Вопросы о важном"
-    private final By questions = By.className("accordion__button");
-
     private final WebDriver driver;
     public MainPage(WebDriver driver) {
         this.driver = driver;
     }
 
-    //Метод получения ответов
-    public String[] getAnswers() {
-        List<WebElement> questionElements = driver.findElements(questions);
-        List<String> answers = new ArrayList<String>() {};
-        for (int i = 0; i < questionElements.size(); i++) {
-            WebElement questionElement = questionElements.get(i);
-            scroll(questionElement);
-            questionElement.click();
-            By answerLocator = By.id("accordion__panel-" + i);
-            WebElement answerElement = driver.findElement(answerLocator);
-            new WebDriverWait(driver, Duration.ofSeconds(WAIT_SECONDS_TIMEOUT))
-                    .until(ExpectedConditions.textMatches(answerLocator, Pattern.compile(".+")));
-            String answer = answerElement.getText();
-            answers.add(answer);
-        }
-
-        return answers.toArray(new String[0]);
+    //Метод получения ответа
+    public String getAnswer(int index) {
+        WebElement questionElement = driver.findElement(By.id("accordion__heading-" + index));
+        scroll(questionElement);
+        questionElement.click();
+        By answerLocator = By.id("accordion__panel-" + index);
+        WebElement answerElement = driver.findElement(answerLocator);
+        new WebDriverWait(driver, Duration.ofSeconds(WAIT_SECONDS_TIMEOUT))
+                .until(ExpectedConditions.textMatches(answerLocator, Pattern.compile(".+")));
+        return answerElement.getText();
     }
 
     //Метод клика по одной из кнопок "Заказать"
--- src/test/java/TestCheckAnswers.java
+++ src/test/java/TestCheckAnswers.java
@@ -2,13 +2,38 @@ import expansion.WebDriverFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import pages.MainPage;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(Parameterized.class)
 public class TestCheckAnswers {
     private WebDriver driver;
+
+    private final int index;
+    private final String answer;
+
+    public TestCheckAnswers(int index, String answer) {
+        this.index = index;
+        this.answer = answer;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getAnswersData() {
+        return new Object[][] {
+                {0, "Сутки — 400 рублей. Оплата курьеру — наличными или картой."},
+                {1, "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим."},
+                {2, "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30."},
+                {3, "Только начиная с завтрашнего дня. Но скоро станем расторопнее."},
+                {4, "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010."},
+                {5, "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится."},
+                {6, "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои."},
+                {7, "Да, обязательно. Всем самокатов! И Москве, и Московской области."},
+        };
+    }
 
     @Before
     public void setup() {
@@ -16,26 +41,11 @@ public class TestCheckAnswers {
     }
 
     @Test
-    public void checkQuestions() {
+    public void checkQuestion() {
         MainPage objMainPage= new MainPage(driver);
 
-        String[] expectedAnswers = new String[] {
-                "Сутки — 400 рублей. Оплата курьеру — наличными или картой.",
-                "Пока что у нас так: один заказ — один самокат. Если хотите покататься с друзьями, можете просто сделать несколько заказов — один за другим.",
-                "Допустим, вы оформляете заказ на 8 мая. Мы привозим самокат 8 мая в течение дня. Отсчёт времени аренды начинается с момента, когда вы оплатите заказ курьеру. Если мы привезли самокат 8 мая в 20:30, суточная аренда закончится 9 мая в 20:30.",
-                "Только начиная с завтрашнего дня. Но скоро станем расторопнее.",
-                "Пока что нет! Но если что-то срочное — всегда можно позвонить в поддержку по красивому номеру 1010.",
-                "Самокат приезжает к вам с полной зарядкой. Этого хватает на восемь суток — даже если будете кататься без передышек и во сне. Зарядка не понадобится.",
-                "Да, пока самокат не привезли. Штрафа не будет, объяснительной записки тоже не попросим. Все же свои.",
-                "Да, обязательно. Всем самокатов! И Москве, и Московской области."
-        };
-
-        String[] answers = objMainPage.getAnswers();
-        for (int i = 0; i < answers.length; i++) {
-            String answer = answers[i];
-            String expected = expectedAnswers[i];
-            assertEquals(expected, answer);
-        }
+        String actual = objMainPage.getAnswer(index);
+        assertEquals(answer,actual);
     }
 
     @After
